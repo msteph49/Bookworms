6b33a37c262e074936071feaaa84a82fe3348b22
Michael Stephens <mdstephens26@yahoo.com>
2021-05-09 03:32:35 -0400

    Added conditional functionality for session navbar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,19 @@
 class UsersController < ApplicationController
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user.index
+            redirect_to user_path(@user.id)
+        else
+            render :new
+        end
+    end
+
+    def show
+        
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  validates :usermane, :uniqueness
+#   validates :usermane, :uniqueness
+    # validates :email, :uniqueness
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :lockable
     has_many :reviews
